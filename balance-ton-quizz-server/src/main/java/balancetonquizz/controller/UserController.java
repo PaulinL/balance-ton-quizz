0ab99be02f9a9d38b21315bedea9f10b79561478
Etienne <etienne.berrie@u-psud.fr>
2020-05-19 11:15:15 +0200

    NEW Add User registration

---
+++ balance-ton-quizz-server/src/main/java/balancetonquizz/controller/UserController.java
@@ -0,0 +1,24 @@
+package balancetonquizz.controller;
+
+import balancetonquizz.dto.UserDto;
+import balancetonquizz.exception.UserAlreadyExistException;
+import balancetonquizz.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    public UserService userService;
+
+    @PostMapping(value = "/register")
+    public ResponseEntity Register(@RequestBody UserDto userDto) throws UserAlreadyExistException {
+        userService.registerNewUserAccount(userDto);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+}
