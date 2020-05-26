5b5f911cbb4f2bfd2500d72165343b119c6fc07a
Ludo <shifty13626@gmail.com>
2020-05-26 17:40:18 +0200

    Add project c# with config

---
+++ TestBalanceTonQuizz/TestBalanceTonQuizz/Configuration/ConfigLoader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace TestBalanceTonQuizz.Configuration
+{
+    public class ConfigLoader
+    {
+
+        #region Tag label config
+
+        private const string NODE_CONFIG = "Config";
+        private const string NODE_ADDRESS = "Address";
+        private const string NODE_USERNAME = "Username";
+        private const string NODE_PASSWORD = "Password";
+
+        #endregion
+
+        /// <summary>
+        /// To load all configuration in file on Config object
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public Config LoadConfig(string url)
+        {
+            var doc = XDocument.Load(url);
+            var root = doc.Element(NODE_CONFIG);
+            if (root == null)
+            {
+                Console.WriteLine("Can't find root element of config");
+                return null;
+            }
+
+
+            var config = new Config()
+            {
+                Address = root.Element(NODE_CONFIG).Element(NODE_ADDRESS).Value,
+                Username = root.Element(NODE_CONFIG).Element(NODE_USERNAME).Value,
+                Password = root.Element(NODE_CONFIG).Element(NODE_PASSWORD).Value
+            };
+
+            return config;
+        }
+
+    }
+}
---
+++ TestBalanceTonQuizz/TestBalanceTonQuizz/Program.cs
@@ -0,0 +1,42 @@
+﻿using log4net;
+using System;
+using System.IO;
+using System.Linq;
+using TestBalanceTonQuizz.Configuration;
+
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
+
+namespace TestBalanceTonQuizz
+{
+    class Program
+    {
+        private static readonly log4net.ILog _log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static ConfigLoader _configLoader;
+
+        static void Main(string[] args)
+        {
+            if (args.Count() > 0)
+            { 
+                Help();
+                return;
+            }
+
+            // Init log file with execution
+            _log.Info("Start Tests application");
+            
+            _configLoader = new ConfigLoader();
+            var config = _configLoader.LoadConfig(Path.Combine(Environment.CurrentDirectory, "config.xml"));
+
+
+
+            _log.Info("End of test");
+        }
+
+
+        static void Help()
+        {
+            _log.Info("Enter all parameter on file config.xml on tool directory before launch test");
+        }
+
+    }
+}
